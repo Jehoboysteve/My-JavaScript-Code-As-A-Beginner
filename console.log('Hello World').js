1faa29150f231bd74823422fc3f6431c532e86d0
Stephen Omojuyigbe <omojuyigbestephen123@gmail.com>
2022-09-16 19:49:52 +0100

    Javascript Commit

---
+++ console.log('Hello World').js
@@ -0,0 +1,21 @@
+var petDog = "Rex"
+var petCat = "Pepper"
+console.log(petDog);
+console.log(petCat);
+console.log("My pet dog's name is", petDog)
+console.log("My pet cat's name is", petCat)
+var catSound = "purr"
+var dogSound = "woof"
+console.log(petDog, "says", dogSound);
+console.log(petCat, "says", catSound);
+catSound = "meow"
+console.log(petCat, "now says", catSound); 
+
+console.log(2 + 2);
+console.log(5 - 2);
+console.log(5 > 2);
+console.log(8 > 10);
+console.log(5 < 12);
+console.log(5 == 5);
+console.log(5 * 5);
+console.log(15 / 3);
